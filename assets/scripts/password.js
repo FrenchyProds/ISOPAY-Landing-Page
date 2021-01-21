0caf69ec2dd61188e1c9dbb625ff00362e33243a
FrenchyProds <yves-menager.paul@hotmail.com>
2021-01-21 14:42:17 +0100

    First commit
    Initial commit

---
+++ assets/scripts/password.js
@@ -0,0 +1,25 @@
+const PASSWORD = 'password'
+const TEXT = 'text'
+
+const passwordIcon = document.querySelector('.preview_icon')
+const passwordField = document.querySelector('.password_input')
+const eyeIcon = document.querySelector('.fas')
+
+eyeIcon.classList.add('fa-eye')
+
+function togglePassword () {
+  if (passwordField.type === PASSWORD) {
+    passwordField.type = TEXT
+
+    eyeIcon.classList.remove('fa-eye')
+    eyeIcon.classList.add('fa-eye-slash')
+  } else {
+    passwordField.type = PASSWORD
+
+    eyeIcon.classList.add('fa-eye')
+    eyeIcon.classList.remove('fa-eye-slash')
+  } 
+//   console.log(passwordField.type);
+}
+
+passwordIcon.addEventListener('click', togglePassword);
